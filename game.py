4e3509e1258d2930d09c22747813ee78be40583e
Flooo34 <flovolpe@hotmail.fr>
2019-03-26 19:53:14 +0100

    update game.py

---
+++ game.py
@@ -0,0 +1,13 @@
+#! /usr/bin/#!/usr/bin/env python3
+#coding : utf-8
+
+'''
+Game MacGiver Project 3 OpenClassRooom
+type labyrinth
+
+'''
+
+import pygame
+from pygame.locals import *
+
+from class import *
