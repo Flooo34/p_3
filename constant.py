76f91cfa556345609146296c1df3fda4ccbd6dca
Flooo34 <flovolpe@hotmail.fr>
2019-03-26 20:40:46 +0100

    update constant's game

--- constant.py
+++ constant.py
@@ -11,3 +11,16 @@ title_windows = "MAC GIVER LABYRINTH"
 picture_icon = "pictures/MacGiver.png"
 
 #Picture of the games
+base_picture =
+wall_picture =
+depart_picture = "pictures/depart.png"
+finish_picture = "pictures/depart.png"
+ether_picture = "pictures/ether.png"
+guardian_picture = "pictures/Gardien.png"
+mac_picture = "pictures/MacGyver.png"
+needle_picture = "pictures/needle.png"
+plastic_picture = "pictures/plastic.png"
+rip_picture = "pictures/rip.png"
+staircase1_picture = "pictures/staircase1.png"
+staircase2_picture = "pictures/staircase2.png"
+syringe_picture = "pictures/syringe.png"
