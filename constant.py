519bfc0d27215f7d7e0925b9942f2246f2bf5f8f
Flooo34 <flovolpe@hotmail.fr>
2019-03-26 20:19:39 +0100

    update constant.py

--- constant.py
+++ constant.py
@@ -1,6 +1,13 @@
 '''
 Constant for the game
 '''
-
-
+#settings windows
+number_sprite_side = 15
+size_sprite = 30
 side_windows = number_sprite_side * size_sprite
+
+#personalization windows
+title_windows = "MAC GIVER LABYRINTH"
+picture_icon = "pictures/MacGiver.png"
+
+#Picture of the games
