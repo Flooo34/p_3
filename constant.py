cc393888461194e008570b3476e96dfbed1711e1
Flooo34 <flovolpe@hotmail.fr>
2019-03-29 08:36:20 +0100

    change name of some costant to resolve problem of the game

--- constant.py
+++ constant.py
@@ -8,19 +8,21 @@ side_windows = number_sprite_side * size_sprite
 
 #personalization windows
 title_windows = "MAC GIVER LABYRINTH"
-picture_icon = "pictures/MacGiver.png"
+picture_icon = "pictures/MacGyver.png"
 
 #Picture of the games
 background_picture ="pictures/background.jpg"
-wall_picture =
-depart_picture = "pictures/depart.png"
-finish_picture = "pictures/depart.png"
+wall_picture ="pictures/wall.png"
+start_picture = "pictures/start.png"
+finish_picture = "pictures/start.png"
 ether_picture = "pictures/ether.png"
 guardian_picture = "pictures/Gardien.png"
 mac_picture = "pictures/MacGyver.png"
 needle_picture = "pictures/needle.png"
 plastic_picture = "pictures/plastic.png"
 rip_picture = "pictures/rip.png"
-staircase1_picture = "pictures/staircase1.png"
-staircase2_picture = "pictures/staircase2.png"
+staircase_up_picture = "pictures/staircase_up.png"
+staircase_down_picture = "pictures/staircase_down.png"
 syringe_picture = "pictures/syringe.png"
+deat_picture = "pictures/death.png"
+groundtest_picture = "pictures/groundTest.png"
