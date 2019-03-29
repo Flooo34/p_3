e134939ecd0fb069d087cc2cdb2294ec2b89923d
Flooo34 <flovolpe@hotmail.fr>
2019-03-29 08:34:24 +0100

    update game.py to show the windows

--- game.py
+++ game.py
@@ -10,25 +10,29 @@ type labyrinth
 import pygame
 from pygame.locals import *
 
-from class import *
+from classs import *
 from constant import*
 
 # Start pygame
 pygame.init()
 
 #Music
-pygame.mixer.music.load("sound/mc.wav")
-son.play()
-pygame.mixer.music.get_volume(0.3)
+sound = pygame.mixer.music.load("sound/mc.wav")
+pygame.mixer.music.play()
+volume = pygame.mixer.music.get_volume()
+pygame.mixer.music.set_volume(0.05)
 
 #start windows game
 windows = pygame.display.set_mode((side_windows, side_windows))
 icon = pygame.image.load(picture_icon)
 pygame.display.set_caption(title_windows)
 
+
 continue_game = 1
 
 while continue_game:
+
+
     #reduct speed loop
     pygame.time.Clock().tick(30)
     for event in pygame.event.get():
@@ -37,4 +41,13 @@ while continue_game:
             continue_game = 0
 
     pygame.key.set_repeat(400, 30)
-    
+
+    background = pygame.image.load(background_picture).convert()
+    windows.blit(background, (0,0))
+
+    choice = 'map.txt'
+    nivel = Game(choice)
+    nivel.generate()
+    nivel.show(windows)
+    icon = pygame.image.load(picture_icon)
+    pygame.display.flip()
